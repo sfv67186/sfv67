3d232c5a770226005c9c739fc694425c45b7d6cb
sfv67 <sfv67186@yahoo.com>
2024-09-30 16:36:18 +0500

    30/09/24

---
+++ variables.py
@@ -0,0 +1,7 @@
+homework = 12
+hours = 1.5
+course = 'Python'
+time_midl = (hours*60) / homework
+print ("Курс:",course, ', всего задач:', homework, ', затрачено часов:', hours, ', среднее время выполнения ', time_midl, ' минут.')
+time_ch = (hours) / homework
+print ("Курс:",course, ', всего задач:', homework, ', затрачено часов:', hours, ', среднее время выполнения ', time_ch, ' часа.')
