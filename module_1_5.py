6fcb2fa5fc1c1ecf3b95177a9294d50a6758091a
sfv67 <sfv67186@yahoo.com>
2024-10-03 10:40:19 +0500

    01/10/24

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = (1, 'temp', True)
+print (immutable_var)
+
+# immutable_var [0] = 2
+
+# Кортежи поддержтвают только операции конкатенацию, умножение и замену элементов внутреннего списка
+# являющегося элементом кортежа.
+
+mutable_list = [3, 'три', False]
+mutable_list [1] = 'четыре'
+print (mutable_list)
