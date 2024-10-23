3c0ff146be6e6503b3777392702527c86331a027
sfv67 <sfv67186@yahoo.com>
2024-10-23 13:24:06 +0500

    2_5

--- module_2_5.py
+++ module_2_5.py
@@ -2,12 +2,15 @@ def get_matrix(n,m,value) :
     matrix = []
     matrix_2 = []
     for i in range (n) :
-        matrix.append(matrix_2)
+        matrix.append ( matrix_2 )
     for j in range (m) :
-        matrix_2.append(value)
+        if value <= 0 :
+            matrix_2.append ( None )
+        else :
+            matrix_2.append ( value )
     return matrix
-result_1 = get_matrix(3, 4, 5)
-result_2 = get_matrix(4, 3, 2)
+result_1 = get_matrix(3, 4, 0)
+result_2 = get_matrix(4, 3, -1)
 result_3 = get_matrix(5, 1, 3)
 print (result_1)
 print (result_2)
